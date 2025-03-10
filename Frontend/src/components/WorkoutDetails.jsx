314000182eec8194f4d02b781ffefa1c72814cbd
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-03-10 16:12:56 +0100

    feat: updated my local url to my deployed one

--- Frontend/src/components/WorkoutDetails.jsx
+++ Frontend/src/components/WorkoutDetails.jsx
@@ -9,7 +9,7 @@ const WorkoutDetails = ({ workout }) => {
   const {dispatch} = useWorkoutsContext( )
   
   const handleClick = async () => {
-    const response = await fetch('/api/workouts/' + workout._id, {
+    const response = await fetch('https://gym-pal.onrender.com/api/workouts/' + workout._id, {
       method: 'DELETE'
     })
 
